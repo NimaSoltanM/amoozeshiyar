5a0e4c8edd12cf17b238c051745e9aac8d7e86f0
NimaSoltanM <46854040+NimaSoltanM@users.noreply.github.com>
2022-12-17 05:59:04 +0330

    Add files via upload

--- components/main-page/Unknown.jsx
+++ components/main-page/Unknown.jsx
@@ -1,14 +1,19 @@
-import { Paper, Divider, Text, Center } from '@mantine/core';
-import Image from 'next/image';
+import { Paper, Divider, Text, Skeleton } from '@mantine/core';
 
 const Unknown = () => {
   return (
-    <Paper shadow='xl' p='md' pb='xs' mih='21.7rem' mah='21.7rem'>
+    <Paper shadow='xl' p='md' pb='xs' mih='21.7rem' mah='21.7rem' color='cyan'>
       <Text mb='sm'>در دست ساخت</Text>
       <Divider mb='xl' />
-      <Center>
-        <Image src='/images/cons.jpg' height={250} width={335} />
-      </Center>
+      <>
+        <Skeleton animate={false} height={50} circle mb='xl' />
+        <Skeleton animate={false} height={8} radius='xl' />
+        <Skeleton animate={false} height={8} mt={6} radius='xl' />
+        <Skeleton animate={false} height={8} mt={6} radius='xl' />
+        <Skeleton animate={false} height={8} mt={6} radius='xl' />
+        <Skeleton animate={false} height={8} mt={6} radius='xl' />
+        <Skeleton animate={false} height={8} mt={6} width='70%' radius='xl' />
+      </>
     </Paper>
   );
 };
